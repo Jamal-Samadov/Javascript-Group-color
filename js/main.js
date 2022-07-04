a0e4a12efa469a75cbc3af20f9246676e445640a
Jamaltscode <jamalts@code.edu.az>
2022-07-04 09:36:29 +0400

    Group-color task done

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+   
+var group = +'P324'
+
+function change(){
+    var value = parseInt(document.getElementById('input').value);
+    var body = document.body
+
+    if(input.substring(1, 2) == 3 ){
+        body.style.backgroundColor = 'black'
+    }
+    else if(input.substring(1, 2) == 2){
+        body.style.backgroundColor = 'red'
+    }
+    else if(input.substring(1, 2) === 1){
+        body.style.backgroundColor = 'yellow'
+    }
+}
+
+
+
+  
